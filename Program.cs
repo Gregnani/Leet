5d14f5c0adef51b100aa77d3c8edbb77476c1dd9
gregnani <fgregnani@gmail.com>
2023-06-04 18:47:28 -0300

    atividade concluida

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿string fraseDigitada, fraseCorrigida;
+
+Console.Write("Digite uma frase:");
+
+fraseDigitada = Console.ReadLine()!;
+
+fraseCorrigida = fraseDigitada;
+
+    .replace("A", "4")
+    .replace("a", "4")
+    .replace("E", "3")
+    .replace("e", "3")
+    .replace("I", "1")
+    .replace("i", "1")
+    .replace("O", "0")
+    .replace("o", "0")
+    .replace("T", "7")
+    .replace("t", "7")
+    .replace("S", "5")
+    .replace("s", "5");
+
+Console.Write($"TEXTO TRADUZIDO {fraseCorrigida}");
+
